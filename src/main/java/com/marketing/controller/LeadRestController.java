1e410916fc9edc82b38b795041f65cb85eaed7ed
gufranahmad01 <er.gufranahmad422@gmail.com>
2022-12-02 22:39:55 +0530

    marketingapp7 backup

---
+++ src/main/java/com/marketing/controller/LeadRestController.java
@@ -0,0 +1,57 @@
+package com.marketing.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marketing.entities.Lead;
+import com.marketing.repositories.LeadRepository;
+
+@RestController
+@RequestMapping("/api/leads")
+public class LeadRestController {
+    @Autowired
+	private LeadRepository leadRepo;
+    
+    //http://localhost:8080/api/leads
+    @GetMapping
+    public List<Lead> getAllLeads(){
+    	List<Lead> leads = leadRepo.findAll();
+    	return leads;
+    }
+    
+    @PostMapping
+    public void saveOneLead(@RequestBody Lead lead) {
+    	leadRepo.save(lead);
+    }
+    
+    @PutMapping
+    public void updateOneLead(@RequestBody Lead lead) {
+    	leadRepo.save(lead);
+    }
+    
+    //http://localhost:8080/api/leads/delete/6
+    @DeleteMapping("/delete/{id}")
+    public void deleteOneLead(@PathVariable("id") long id) {
+    	leadRepo.deleteById(id);
+    }
+    
+   //EXPOSING OF DATA IN WEB SERVICES.
+  //http://localhost:8080/api/leads/leadinfo/{id}
+    @RequestMapping("/leadinfo/{id}")
+    public Lead getOneLead(@PathVariable("id")long id) {
+    	Optional<Lead> findById = leadRepo.findById(id);
+    	Lead lead = findById.get();
+    	return lead;
+    }
+    
+}
